f72c845bb8ee71aa8e07c15e88bc3225f099d132
DraganDosen <dragan.dosen20@gmail.com>
2024-11-25 23:38:59 +0100

    Update MethodsPage.py

--- tests/step_definitions/MethodsPage.py
+++ tests/step_definitions/MethodsPage.py
@@ -20,6 +20,7 @@ class Methods:
      page = browser.new_page()
      yield page 
      page.close()
+     ###test
 
     @given ("As an not logged user navigate to homepage https://www.kiwi.com/en/")
     def go_to_page(page):
